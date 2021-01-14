720367100be75def723632dee425422ed54a06a4
Muhammad Nur Ichsan <mnichsaan@gmail.com>
2021-01-14 22:05:38 +0700

    made laundry details and history transaction page

--- src/views/user/LaundryDetail.vue
+++ src/views/user/LaundryDetail.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="home">
-    <Header/>
-    <Photo/>
-    <Footer/>
-  </div>
+	<div class="home">
+		<Header />
+		<Gallery />
+		<Footer />
+	</div>
 </template>
 
 <script>
-import Header from '@/components/Header'
-import Photo from '@/components/Photo'
-import Footer from '@/components/Footer'
+	import Header from "@/components/Header";
+	import Gallery from "@/components/Gallery";
+	import Footer from "@/components/Footer";
 
-export default {
-  name: 'LaundryDetail',
-  components: {
-    Header,
-    Photo,
-    Footer
-  }
-}
+	export default {
+		name: "LaundryDetail",
+		components: {
+			Header,
+			Gallery,
+			Footer,
+		},
+	};
 </script>
 <style scoped>
-.photo-sec {
-  height: 500px;
-}
+	.photo-sec {
+		height: 500px;
+	}
 </style>
