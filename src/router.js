a653d2038d02fa35a7699ac9beca79770aaa54a1
akbarsaputrait <akbarsaputrait@outlook.com>
2021-01-15 18:19:16 +0700

    Fix Lint

--- src/router.js
+++ src/router.js
@@ -7,111 +7,116 @@ import Guest from '@/utils/Guest'
 
 Vue.use(Router)
 
-const routes = [{
-        path: '/',
-        component: () =>
-            import ('./views/user/Home'),
-        meta: {
-            requiresAuth: false
-        }
-    },
-    {
-        path: '/app',
-        component: () =>
-            import ( /* webpackChunkName: "app" */ './views/app'),
-        beforeEnter: AuthAdminRequired,
-        redirect: '/app/admin/dashboard',
-        children: [{
-            path: 'admin',
-            component: () =>
-                import ( /* webpackChunkName: "admin" */ './views/app/admin/index'),
-            // beforeEnter: AuthAdminRequired,
-            redirect: '/app/admin/dashboard',
-            children: [{
-                    path: 'dashboard',
-                    component: () =>
-                        import ( /* webpackChunkName: "admin" */ './views/app/admin/Dashboard')
-                },
-                {
-                    path: 'transaction',
-                    component: () =>
-                        import ( /* webpackChunkName: "admin" */ './views/app/admin/Transaction')
-                }
-            ]
-        }]
-    },
-    {
-        path: '/user',
-        component: () =>
-            import ( /* webpackChunkName: "user" */ './views/user/index'),
-        beforeEnter: AuthUserRequired,
-        children: [{
-                path: '/laundry_detail/',
-                name: 'LaundryDetail',
-                props: true,
-                component: () =>
-                    import ( /* webpackChunkName: "user" */ './views/user/LaundryDetail.vue')
-            },
-            {
-                path: '/order',
-                name: 'OrderForm',
-                props: true,
-                component: () =>
-                    import ( /* webpackChunkName: "user" */ './views/user/OrderForm.vue')
-            },
-            {
-                path: '/success',
-                name: 'Success',
-                component: () =>
-                    import ( /* webpackChunkName: "user" */ './views/user/Success.vue')
-            },
-            {
-                path: '/history',
-                name: 'History',
-                component: () =>
-                    import ( /* webpackChunkName: "user" */ './views/user/History.vue')
-            }
-        ]
-    },
-    {
-        path: '/error',
-        component: () =>
-            import ( /* webpackChunkName: "error" */ './views/Error')
-    },
-    {
-        path: '/auth',
-        component: () =>
-            import ( /* webpackChunkName: "auth" */ './views/auth'),
-        redirect: '/auth/login',
-        beforeEnter: Guest,
-        children: [{
-                path: 'login',
-                component: () =>
-                    import ( /* webpackChunkName: "auth" */ './views/auth/Login')
-            },
-            {
-                path: 'register',
-                component: () =>
-                    import ( /* webpackChunkName: "auth" */ './views/auth/Register')
-            },
-            {
-                path: 'forgot-password',
-                component: () =>
-                    import ( /* webpackChunkName: "auth" */ './views/auth/ForgotPassword')
-            }
-        ]
-    },
-    {
-        path: '*',
-        component: () =>
-            import ( /* webpackChunkName: "error" */ './views/Error')
+const routes = [
+  {
+    path: '/',
+    component: () =>
+      import ('./views/user/Home'),
+    meta: {
+      requiresAuth: false
     }
+  },
+  {
+    path: '/app',
+    component: () =>
+      import (/* webpackChunkName: "app" */ './views/app'),
+    beforeEnter: AuthAdminRequired,
+    redirect: '/app/admin/dashboard',
+    children: [
+      {
+        path: 'admin',
+        component: () =>
+          import (/* webpackChunkName: "admin" */ './views/app/admin/index'),
+        // beforeEnter: AuthAdminRequired,
+        redirect: '/app/admin/dashboard',
+        children: [
+          {
+            path: 'dashboard',
+            component: () =>
+              import (/* webpackChunkName: "admin" */ './views/app/admin/Dashboard')
+          },
+          {
+            path: 'transaction',
+            component: () =>
+              import (/* webpackChunkName: "admin" */ './views/app/admin/Transaction')
+          }
+        ]
+      }]
+  },
+  {
+    path: '/user',
+    component: () =>
+      import (/* webpackChunkName: "user" */ './views/user/index'),
+    beforeEnter: AuthUserRequired,
+    children: [
+      {
+        path: '/laundry_detail/',
+        name: 'LaundryDetail',
+        props: true,
+        component: () =>
+          import (/* webpackChunkName: "user" */ './views/user/LaundryDetail.vue')
+      },
+      {
+        path: '/order',
+        name: 'OrderForm',
+        props: true,
+        component: () =>
+          import (/* webpackChunkName: "user" */ './views/user/OrderForm.vue')
+      },
+      {
+        path: '/success',
+        name: 'Success',
+        component: () =>
+          import (/* webpackChunkName: "user" */ './views/user/Success.vue')
+      },
+      {
+        path: '/history',
+        name: 'History',
+        component: () =>
+          import (/* webpackChunkName: "user" */ './views/user/History.vue')
+      }
+    ]
+  },
+  {
+    path: '/error',
+    component: () =>
+      import (/* webpackChunkName: "error" */ './views/Error')
+  },
+  {
+    path: '/auth',
+    component: () =>
+      import (/* webpackChunkName: "auth" */ './views/auth'),
+    redirect: '/auth/login',
+    beforeEnter: Guest,
+    children: [
+      {
+        path: 'login',
+        component: () =>
+          import (/* webpackChunkName: "auth" */ './views/auth/Login')
+      },
+      {
+        path: 'register',
+        component: () =>
+          import (/* webpackChunkName: "auth" */ './views/auth/Register')
+      },
+      {
+        path: 'forgot-password',
+        component: () =>
+          import (/* webpackChunkName: "auth" */ './views/auth/ForgotPassword')
+      }
+    ]
+  },
+  {
+    path: '*',
+    component: () =>
+      import (/* webpackChunkName: "error" */ './views/Error')
+  }
 ]
 
 const router = new Router({
-    linkActiveClass: 'active',
-    routes,
-    mode: 'history'
+  linkActiveClass: 'active',
+  routes,
+  mode: 'history'
 })
 
 export default router
--- src/views/user/LaundryDetail.vue
+++ src/views/user/LaundryDetail.vue
@@ -1,27 +1,27 @@
 <template>
-	<div class="home">
-		<Header />
-		<Gallery />
-		<Footer />
-	</div>
+  <div class="home">
+    <Header/>
+    <Gallery/>
+    <Footer/>
+  </div>
 </template>
 
 <script>
-	import Header from "@/components/Header";
-	import Gallery from "@/components/Gallery";
-	import Footer from "@/components/Footer";
+import Header from '@/components/Header'
+import Gallery from '@/components/Gallery'
+import Footer from '@/components/Footer'
 
-	export default {
-		name: "LaundryDetail",
-		components: {
-			Header,
-			Gallery,
-			Footer,
-		},
-	};
+export default {
+  name: 'LaundryDetail',
+  components: {
+    Header,
+    Gallery,
+    Footer
+  }
+}
 </script>
 <style scoped>
-	.photo-sec {
-		height: 500px;
-	}
+.photo-sec {
+  height: 500px;
+}
 </style>
